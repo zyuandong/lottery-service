878f753e422eea0945121349c75fb5dc9b82c6a2
zyuandong <del1719@163.com>
2021-08-26 02:16:24 +0800

    [prize] see: api

--- routes/prizes.js
+++ routes/prizes.js
@@ -2,16 +2,20 @@ const router = require('koa-router')();
 const sql = require('../utils/sql');
 const { v5: uuidv5 } = require('uuid');
 
-router.prefix('/prizes')
+router.prefix('/prizes');
 
 router
   .post('/add', async (ctx) => {
-    let data = await sql.insert('prize')
-    ctx.body = data
+    let data = await sql.insert('prize');
+    ctx.body = data;
   })
   .get('/', async (ctx) => {
-    let data = await sql.queryAll('prize')
-    ctx.body = data
-  })
+    let data = await sql.queryAll('prize');
+    ctx.body = data;
+  });
+  // update active
+  // update probability
+  // upload pic
+  // set prize pools
 
 module.exports = router;
