848709d375037fff22918897b5c6c34b84dedbf6
zyuandong <del1719@163.com>
2021-08-28 20:58:05 +0800

    [awardRecord] feat: latestAwardRecord

--- routes/awardRecord.js
+++ routes/awardRecord.js
@@ -1,12 +1,39 @@
 const router = require('koa-router')();
+const sd = require('silly-datetime');
 const sql = require('../utils/sql');
+const runSql = require('../utils/runSql');
 
 router.prefix('/award_records');
+
+const getLatestAwardRecord = () => {
+  return new Promise((resolve, reject) => {
+    let str = `select * from award_record where create_time >= '${sd.format(
+      new Date() - 24 * 3600 * 1000,
+      'YYYY-MM-DD HH:mm:ss'
+    )}' order by create_time desc`;
+    runSql(
+      str,
+      (res) => {
+        resolve({
+          code: 200,
+          message: 'success',
+          data: res,
+        });
+      },
+      (err) => reject(err)
+    );
+  });
+};
 
 router.get('/', async (ctx) => {
   const data = await sql.queryAll('award_record', ctx.request.query, [
     { field: 'create_time', type: 'desc' },
   ]);
+  ctx.body = data;
+});
+
+router.get('/latest', async (ctx) => {
+  const data = await getLatestAwardRecord();
   ctx.body = data;
 });
 
