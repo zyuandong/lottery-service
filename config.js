20337a3c3d6746538dfe979151019389442d56cd
zyuandong <del1719@163.com>
2022-10-13 16:20:21 +0800

    add: pnpm / .prettierrc.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
   port: 3000,
   database: {
-    host: '47.96.178.84',
+    host: 'localhost',
     port: '3306',
     user: 'root',
     password: '12345678',
-    database: 'lottery_service'
-  }
-}
+    database: 'lottery_service',
+  },
+};
 
-module.exports = config
+module.exports = config;
